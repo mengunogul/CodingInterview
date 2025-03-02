2c09d50d00a1c5bdd524577e2d1092992cb07aa4
Berkay <berkay.mengunogul@outlook.com>
2025-03-02 16:26:01 -0600

    Arrays - Neetcode 1 - Add solution for detecting duplicates in an array

---
+++ neetcode/arrays_and_hashing/1_contain_duplicate.py
@@ -0,0 +1,10 @@
+"""
+Question: https://neetcode.io/problems/duplicate-integer
+"""
+
+from typing import List
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
