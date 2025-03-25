ad044ad51c922eeccf1a4ad952601450b7f78021
Berkay <berkay.mengunogul@outlook.com>
2025-03-25 02:26:10 -0500

    Heap - Neetcode 67 - Add solution for Find Median in a Data Stream problem

---
+++ neetcode/heap/67_find_median.py
@@ -0,0 +1,20 @@
+"""
+Question: https://neetcode.io/problems/find-median-in-a-data-stream
+"""
+
+
+class MedianFinder:
+    def __init__(self):
+        self.data = []
+
+    def addNum(self, num: int) -> None:
+        self.data.append(num)
+
+    def findMedian(self) -> float:
+        self.data.sort()
+        n = len(self.data)
+        return (
+            self.data[n // 2]
+            if (n & 1)
+            else (self.data[n // 2] + self.data[n // 2 - 1]) / 2
+        )
