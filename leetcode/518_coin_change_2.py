445174d36d4e875225f871247197b2b76fce5ca5
Berkay <berkay.mengunogul@outlook.com>
2025-06-28 23:17:03 -0500

    feat: Implement solution for Leetcode 518 - Coin Change II problem

---
+++ leetcode/518_coin_change_2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] += dp[a - coin]
+
+        return dp[-1]
