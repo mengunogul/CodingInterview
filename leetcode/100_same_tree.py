bb4c9cadc87faba1d2cc2fa7db5c49937807ab5e
Berkay <berkay.mengunogul@outlook.com>
2025-07-06 13:52:01 -0500

    feat: Implement solution for Leetcode 100 - Same Tree problem

---
+++ leetcode/100_same_tree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if (p and not q) or (not p and q):
+            return False
+        if p.val != q.val:
+            return False
+
+        if not self.isSameTree(p.left, q.left):
+            return False
+        if not self.isSameTree(p.right, q.right):
+            return False
+
+        return True
